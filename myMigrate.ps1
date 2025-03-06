69b406999cb5ba0d5d4599dbc0a72294e890457f
andrey <andrey.kozhevnikov@devexpress.com>
2025-03-06 16:10:49 +0400

    ww

--- myMigrate.ps1
+++
@@ -1,4 +0,0 @@
-install-package Microsoft.EntityFrameworkCore.Tools -version 6.0.3 -ProjectName MySQLSolution.Module
-
-Add-Migration InitialCreate -StartupProject MySQLSolution.Module -Project MySQLSolution.Module
-Update-Database -StartupProject MySQLSolution.Module -Project MySQLSolution.Module
